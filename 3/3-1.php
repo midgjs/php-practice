659c59bcd5f7226df5abb4deb6146c54e04e9119
midgjs <sjqksk.10@gmail.com>
2022-10-23 16:25:30 +0900

    3

---
+++ 3/3-1.php
@@ -0,0 +1,68 @@
+<?php
+if(100.00 - 100)
+{
+    print "true<br>";
+} else{
+    print "false<br>";
+}  
+
+if("zero")
+{
+    print "true<br>";
+} else{
+    print "false<br>";
+}
+
+if("false")
+{
+    print "true<br>";
+} else{
+    print "false<br>";
+}
+
+// if(0 + "true") //0
+// {
+//     print "true<br>";
+// } else{
+//     print "false<br>";
+// }
+
+if(0.000)
+{
+    print "true<br>";
+} else{
+    print "false<br>";
+}
+
+if("0.0")
+{
+    print "true<br>";
+} else{
+    print "false<br>";
+}
+
+if(strcmp("false","False"))
+{
+    print "true<br>";
+} else{
+    print "false<br>";
+}
+
+if(0 <=> 0) //왼쪽이 크면 양수 작으면 음수 같으면 0 숫자0은 false
+{
+    print "true<br>";
+} else{
+    print "false<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
